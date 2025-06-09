e6ed2336e2e86b52c6a74f34a11b8585db2d4bce
ics-oka <oka@ics-web@gmail.com>
2025-06-09 18:02:40 +0900

    p5デモの実装

---
+++ sketch.js
@@ -0,0 +1,45 @@
+/* 1. 変数の定義 */
+const NUM_POINTS = 100;
+const MAX_LINE_DIST = 100;
+let points = [];
+
+function setup() {
+  /* 2. キャンバスの初期化 */
+  createCanvas(640, 640);
+  background(0);
+  stroke(255, 100);
+
+  /* 3. 点群の初期化 */
+  for (let i = 0; i < NUM_POINTS; i++) {
+    const point = {
+        x: random(width),
+        y: random(height),
+        vx: random(-1, 1),
+        vy: random(-1, 1),
+    };
+    points.push(point);
+  }
+}
+
+function draw() {
+  background(0);
+
+  /* 4. 点群のアップデート */
+  for (let p of points) {
+    p.x += p.vx;
+    p.y += p.vy;
+    if (p.x < 0 || p.x > width) p.vx *= -1;
+    if (p.y < 0 || p.y > height) p.vy *= -1;
+    point(p.x, p.y);
+  }
+
+  /* 5. 点を線で繋ぐ */
+  for (let i = 0; i < points.length; i++) {
+    for (let j = i + 1; j < points.length; j++) {
+      let d = dist(points[i].x, points[i].y, points[j].x, points[j].y);
+      if (d < MAX_LINE_DIST) {
+        line(points[i].x, points[i].y, points[j].x, points[j].y);
+      }
+    }
+  }
+}
