cd6276d0104a1612553805f1394400a81c5a94d0
ics-oka <oka@ics-web@gmail.com>
2025-06-10 13:41:39 +0900

    let -> const

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 /* 1. 変数の定義 */
 const NUM_POINTS = 100;
 const MAX_LINE_DIST = 100;
-let points = [];
+const points = [];
 
 function setup() {
   /* 2. キャンバスの初期化 */
@@ -25,7 +25,7 @@ function draw() {
   background(0);
 
   /* 4. 点群のアップデート */
-  for (let p of points) {
+  for (const p of points) {
     p.x += p.vx;
     p.y += p.vy;
     if (p.x < 0 || p.x > width) p.vx *= -1;
@@ -36,7 +36,7 @@ function draw() {
   /* 5. 点を線で繋ぐ */
   for (let i = 0; i < points.length; i++) {
     for (let j = i + 1; j < points.length; j++) {
-      let d = dist(points[i].x, points[i].y, points[j].x, points[j].y);
+      const d = dist(points[i].x, points[i].y, points[j].x, points[j].y);
       if (d < MAX_LINE_DIST) {
         line(points[i].x, points[i].y, points[j].x, points[j].y);
       }
